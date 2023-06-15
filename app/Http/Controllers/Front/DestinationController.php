8b83e708eb3d9eff28fa148e3d7d1fee3cf16259
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2023-06-15 16:40:00 +0200

    updates

--- app/Http/Controllers/Front/DestinationController.php
+++ app/Http/Controllers/Front/DestinationController.php
@@ -86,7 +86,7 @@ class DestinationController extends Controller
         $destination = Destination::where('slug', '=', $slug)->first();
 		$seo = $destination->seo;
 		$destinations = \App\Destination::select('id', 'name')->get();
-		$activities = $destination->activities()->get();
+		$activities = \App\Activity::all();
 		return view('front.destinations.show', compact('destination', 'destinations', 'activities', 'seo'));
 	}
 }
