ee0151fdd57e0fd6b03e4d54f915f8af58dc1139
Bicky Raj Kayastha <bickyrajkayastha@gmail.com>
2023-07-12 02:34:08 +0200

    updates

--- resources/views/front/blogs/show.blade.php
+++ resources/views/front/blogs/show.blade.php
@@ -41,12 +41,11 @@
     </div>
 </section>
 @endif
-
 <!-- similar blogs -->
-@if (isset($blog->similar_blogs) && !empty($similar_blogs)))
+@if (isset($blog->similar_blogs) && !empty($blog->similar_blogs))
 <section class="news mb-5 mt-20 bg-gray">
     <div class="container">
-        <h2 class="relative pr-10 text-3xl font-bold uppercase lg:text-5xl text-gray-600 font-display pb-10 pt-10">Similar Blogs</h2>
+        <h2 class="relative pr-10 text-3xl font-bold uppercase lg:text-5xl text-gray-600 font-display pb-10 pt-10">Latest Travel Blogs</h2>
         <div class="absolute right-0 w-6 h-1 rounded top-1/2 bg-accent"></div>
         <div class="grid lg:grid-cols-3 gap-2 xl:gap-3">
             @forelse ($blog->similar_blogs as $s_blog)
@@ -67,28 +66,4 @@
     </div>
 </section>
 @endif
-
-<!-- Latest News -->
-<section class="news mb-5 mt-20 bg-gray">
-    <div class="container">
-        <h2 class="relative pr-10 text-3xl font-bold uppercase lg:text-5xl text-gray-600 font-display pb-10 pt-10">Latest Travel Blogs</h2>
-        <div class="absolute right-0 w-6 h-1 rounded top-1/2 bg-accent"></div>
-        <div class="grid lg:grid-cols-3 gap-2 xl:gap-3">
-            @forelse ($blogs as $blog)
-                <a href="{{ route('front.blogs.show', ['slug' => $blog->slug]) }}">
-                    <div class="article">
-                        <div class="image">
-                            <img src="{{ $blog->imageUrl }}" alt="">
-                        </div>
-                        <div class="content">
-                            <h2>{{ $blog->name }}</h2>
-                            <p class="fs-sm">{{ truncate(strip_tags($blog->description)) }}</p>
-                        </div>
-                    </div>
-                </a>
-            @empty
-            @endforelse
-        </div>
-    </div>
-</section>
 @endsection
